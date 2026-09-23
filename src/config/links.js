bffd2667259df8b7bb7e8317913cb9f2c12a6cf5
Abhiram220107 <abhiramgadde2007@gmail.com>
2026-09-23 23:17:57 +0530

    Update rules link to Google Docs

--- src/config/links.js
+++ src/config/links.js
@@ -17,7 +17,7 @@ export const LINKS = {
   attendance: "https://attenza-by-abhiram.vercel.app",
 
   // 📜 3. Rules & Regulations
-  rules: "https://example.com/rules-and-regulations"
+  rules: "https://docs.google.com/document/d/1mSb8yvkf3DDihuJcab3y_bVZTXRcpUZpcn7MM5UQvNY/edit?usp=drive_link"
 };
 
 export default LINKS;
